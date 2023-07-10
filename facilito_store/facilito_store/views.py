f99c59d6e401818fd78396b3460fabd36c8720db
maicol <maicolmike@hotmail.com>
2023-07-10 17:07:18 -0500

    segundo dia de trabajo

--- facilito_store/facilito_store/views.py
+++ facilito_store/facilito_store/views.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hola mundo")
+    # return HttpResponse("Hola mundo") # funciona con rom django.http import HttpResponse
+    return render(request, 'index.html',{
+        'mensaje':"Listado de productos",
+        'title': "Index dinamico",
+        'products':[{'title':'playera', 'price':5, 'stock':True},
+                    {'title':'camiseta', 'price':4, 'stock':False},
+                    ]
+    })
