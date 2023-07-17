5c7b60e4575b7aceb40bcb9e19304496e2db90b3
maicol <maicolmike@hotmail.com>
2023-07-17 17:24:32 -0500

    se configuro los productos en forma de clase

--- facilito_store/facilito_store/urls.py
+++ facilito_store/facilito_store/urls.py
@@ -4,10 +4,13 @@ from django.urls import path
 
 from . import views
 
+from products.views import ProductListView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     #definir url
-    path('',views.index, name='index'),
+    #path('',views.index, name='index'),
+    path('',ProductListView.as_view(), name='index'),
     path('usuarios/login',views.login_view, name='login'),
     path('usuarios/logout',views.logout_view, name='logout'),
     path('usuarios/registro',views.register, name='register'),
