d900e30f3b5f9b22f39da0439495fd5a93646776
maicol <maicolmike@hotmail.com>
2023-07-19 17:26:32 -0500

    se realizo modificaciones al modelo y se puso la configuracion del slug

--- facilito_store/facilito_store/urls.py
+++ facilito_store/facilito_store/urls.py
@@ -1,6 +1,7 @@
-
 from django.contrib import admin
+
 from django.urls import path
+from django.urls import include
 
 from . import views
 
@@ -14,4 +15,5 @@ urlpatterns = [
     path('usuarios/login',views.login_view, name='login'),
     path('usuarios/logout',views.logout_view, name='logout'),
     path('usuarios/registro',views.register, name='register'),
+    path('productos/', include('products.urls'))
 ]
--- facilito_store/products/views.py
+++ facilito_store/products/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
 from .models import Product
 
 class ProductListView(ListView):
@@ -12,5 +14,15 @@ class ProductListView(ListView):
         context = super().get_context_data(**kwargs)
         context['message'] = 'Listado de productos'
         context['products']=context['product_list']
+
+        return context
+    
+class ProductDetailView(DetailView): #id - pk
+    model = Product
+    template_name = 'products/product.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        print(context)
 
         return context
