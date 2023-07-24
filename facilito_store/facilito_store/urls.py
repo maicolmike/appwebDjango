2464acd446e264c78e0801762f05038eccf5bda6
maicol <maicolmike@hotmail.com>
2023-07-24 17:29:41 -0500

    manipulando imagenes

--- facilito_store/facilito_store/urls.py
+++ facilito_store/facilito_store/urls.py
@@ -7,6 +7,9 @@ from . import views
 
 from products.views import ProductListView
 
+from django.conf.urls.static import static #por incluir la carpeta media
+from django.conf import settings #por incluir la carpeta media
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     #definir url
@@ -17,3 +20,8 @@ urlpatterns = [
     path('usuarios/registro',views.register, name='register'),
     path('productos/', include('products.urls'))
 ]
+
+#condicion para mostrar las imagenes en nuestro templates
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    
--- facilito_store/products/admin.py
+++ facilito_store/products/admin.py
@@ -5,7 +5,7 @@ from .models import Product
 
 #poder configurar que cuando se agrege un producto solo aparescan estos atributos
 class ProductAdmin(admin.ModelAdmin):
-    fields = ('title', 'description', 'price')
+    fields = ('title', 'description', 'price','image')
     list_display = ('__str__','slug','created_at')
 
 admin.site.register(Product,ProductAdmin)
