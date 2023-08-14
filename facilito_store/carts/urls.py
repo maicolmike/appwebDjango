5cfd5c322434fdb80cb510a613a359aabbb1fb54
maicol <maicolmike@hotmail.com>
2023-08-14 17:24:44 -0500

    se trabaja en el carrito de compras

---
+++ facilito_store/carts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'carts' #para establecer rutas
+
+urlpatterns = [
+    #path('<pk>',views.ProductDetailView.as_view(), name='product'), #id -> llave primaria
+    path('', views.cart, name='cart')
+]
--- facilito_store/carts/views.py
+++ facilito_store/carts/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Cart
+
+def cart (request):
+    #crear la session
+    ##request.session['cart_id'] = '123'
+    
+    #obtenet la informacion
+    #valor = request.session.get('cart_id')
+    #print (valor)
+    
+    #eliminar una sesion 
+    #request.session['cart_id'] = None
+    
+    # obtener usuario autenticado y validar
+    user = request.user if request.user.is_authenticated else None
+    cart_id = request.session.get('cart_id')
+    
+    if cart_id:
+        cart = Cart.objects.get(pk=cart_id) #obtenemos el carrito de la base de datos
+    else:
+        cart = Cart.objects.create(user=user) # generamos el carrito de compras
+    
+    request.session['cart_id']= cart.id #actualizar la sesion
+    
+    return render(request,'carts/cart.html',{
+    })
