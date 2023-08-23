4ad0bbf019b69ba5f80c6f3fba73bc680ffb8325
maicolmike <maicolmike@hotmail.com>
2023-08-23 17:28:42 -0500

    agregar y eliminar productos al carrito de compras

--- facilito_store/carts/urls.py
+++ facilito_store/carts/urls.py
@@ -6,5 +6,7 @@ app_name = 'carts' #para establecer rutas
 
 urlpatterns = [
     #path('<pk>',views.ProductDetailView.as_view(), name='product'), #id -> llave primaria
-    path('', views.cart, name='cart')
+    path('', views.cart, name='cart'),
+    path ('agregar', views.add, name='add'),
+    path ('eliminar', views.remove, name='remove'),
 ]
--- facilito_store/carts/views.py
+++ facilito_store/carts/views.py
@@ -1,14 +1,37 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 # Create your views here.
 from .models import Cart
 from .utils import get_or_create_cart
+from products.models import Product
 
 def cart (request):
     cart = get_or_create_cart(request)
     
     return render(request,'carts/cart.html',{
+        'cart':cart
     })
+    
+def add(request):
+    cart = get_or_create_cart(request)
+    product = Product.objects.get(pk=request.POST.get('product_id'))
+    cart.products.add(product)
+    
+    return render(request, 'carts/add.html',{
+        'product':product
+    })
+    
+def remove(request):
+    cart = get_or_create_cart(request)
+    product = Product.objects.get(pk=request.POST.get('product_id'))
+
+    cart.products.remove(product)
+
+    return redirect('carts:cart')
+    
+    
+    
     
     #crear la session
     ##request.session['cart_id'] = '123'
