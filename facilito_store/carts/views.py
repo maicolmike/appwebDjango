382b55540e9669084377973e096222af06be11c0
maicolmike <maicolmike@hotmail.com>
2023-08-29 17:33:42 -0500

    carrito de compras cantidades

--- facilito_store/carts/views.py
+++ facilito_store/carts/views.py
@@ -7,7 +7,7 @@ from .utils import get_or_create_cart
 from products.models import Product
 from django.shortcuts import redirect
 from django.shortcuts import get_object_or_404
-
+from .models import CartProducts
 
 def cart (request):
     cart = get_or_create_cart(request)
@@ -18,8 +18,13 @@ def cart (request):
     
 def add(request):
     cart = get_or_create_cart(request)
-    product = Product.objects.get(pk=request.POST.get('product_id'))
-    cart.products.add(product)
+    product = get_object_or_404(Product,pk=request.POST.get('product_id'))
+    quantity = int(request.POST.get('quantity',1))
+    
+   # cart.products.add(product, through_defaults= {
+    #    'quantity':quantity
+    #})
+    cart_product = CartProducts.objects.create_or_update_quantity(cart=cart, product=product, quantity=quantity)
     
     return render(request, 'carts/add.html',{
         'product':product
