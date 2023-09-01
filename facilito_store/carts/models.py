5c935b39c5d066e03e536c2b71dd439c4418ba4d
maicolmike <maicolmike@hotmail.com>
2023-09-01 17:29:24 -0500

    orden de compra verificar el envio en resumen no esta dando y genera error

--- facilito_store/carts/models.py
+++ facilito_store/carts/models.py
@@ -26,6 +26,9 @@ class Cart(models.Model):
         self.update_subtotal()
         self.update_total()
         
+        if self.order:
+            self.order.update_total()
+        
     def update_subtotal(self):
         self.subtotal = sum([ cp.quantity * cp.product.price for cp in self.products_related() ])
         self.save()
@@ -36,7 +39,11 @@ class Cart(models.Model):
     
     def products_related(self):
         return self.cartproducts_set.select_related('product')
-
+    
+    @property
+    def order(self):
+        return self.order_set.first() 
+    
 class CartProductsManager(models.Manager):
     def create_or_update_quantity(self, cart, product, quantity=1):
         object, created = self.get_or_create(cart=cart, product=product)
