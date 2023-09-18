66a47e14764270904f4f0a5411eab41f0bd3340d
maicolmike <maicolmike@hotmail.com>
2023-09-18 17:31:19 -0500

    direcciones

--- facilito_store/shipping_addresses/views.py
+++ facilito_store/shipping_addresses/views.py
@@ -5,6 +5,9 @@ from django.views.generic import ListView
 from .models import ShippingAddress
 from .forms import ShippingAddressForm
 
+from django.shortcuts import redirect
+from django.contrib import messages
+
 class ShippingAddressListView(ListView):
     #login_url = 'login'
     model = ShippingAddress
@@ -14,7 +17,17 @@ class ShippingAddressListView(ListView):
         return ShippingAddress.objects.filter(user=self.request.user).order_by('-default')
     
 def create(request):
-    form = ShippingAddressForm()
+    form = ShippingAddressForm(request.POST or None)
+    
+    if request.method == 'POST' and form.is_valid():
+        shipping_address = form.save(commit = False) # metodo save se encargara de persisitir objeto
+        shipping_address.user = request.user
+        shipping_address.default = not ShippingAddress.objects.filter(user=request.user).exists() # si no existen direcciones sera default
+        
+        shipping_address.save()
+        messages.success(request,'Dirreccion creada con exito')
+        return redirect('shipping_addresses:shipping_addresses')
+ 
     return render(request, 'shipping_addresses/create.html',{
         'form': form
         
